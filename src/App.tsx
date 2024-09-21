5848e3b298f6cf4b37e7ec24750fec07746705e4
ErildoJS <erildofranciscojs@gmail.com>
2024-09-21 16:10:00 +0100

    fix: center content

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import './App.module.css'
 import Image from './assets/image.png'
 import styles from './App.module.css';
-
+import Google from './assets/google.png'
+import Github from './assets/github.png'
+import Guest from './assets/guest.png'
 function App() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
-        <img src={Image} alt="BookWise Logo" className={styles.logo} />
-      </div>
+      <img src={Image} alt="BookWise Logo" className={styles.logo} />
       <div className={styles.right}>
         <h1>Boas vindas!</h1>
         <p>Faça seu login ou acesse como visitante.</p>
-        <button className={styles.googleButton}>Entrar com Google</button>
-        <button className={styles.githubButton}>Entrar com GitHub</button>
-        <button className={styles.visitorButton}>Acessar como visitante</button>
+        <button className={styles.googleButton}
+        >
+          <img src={Google} className={styles.imagep} alt="" />
+          Entrar com Google</button>
+        <button className={styles.githubButton}>
+          <img className={styles.imagep} src={Github} alt="" />
+          Entrar com GitHub</button>
+        <button className={styles.visitorButton}>
+          <img className={styles.imagep} src={Guest} alt="" />
+          Acessar como visitante</button>
       </div>
     </div>
   )
